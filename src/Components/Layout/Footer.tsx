00d24e829400b08b448a71361bbe6b6a84d34d77
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-12-18 09:17:58 -0600

    Header and Footer Component

---
+++ src/Components/Layout/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer fixed-bottom text-center p-3 bg-dark text-white">
+      &copy;Made with <i className="bi bi-heart-fill"></i> by DotNetMastery
+    </div>
+  );
+}
+
+export default Footer;
--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Footer, Header } from "../Components/Layout";
 
 function App() {
-  return <div className="text-success">App</div>;
+  return (
+    <div>
+      <Header />
+      Main Component
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
