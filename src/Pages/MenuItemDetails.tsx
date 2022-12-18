dc8f3e004d6fd7e4d7d44c6bf477de7eee57a03f
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-12-18 12:24:19 -0600

    Assignment 20 - Load Menu Item Details

--- src/Pages/MenuItemDetails.tsx
+++ src/Pages/MenuItemDetails.tsx
@@ -1,68 +1,87 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useGetMenuItemByIdQuery } from "../Apis/menuItemApi";
+import { useNavigate } from "react-router-dom";
 
 function MenuItemDetails() {
+  const { menuItemId } = useParams();
+  const { data, isLoading } = useGetMenuItemByIdQuery(menuItemId);
+  const navigate = useNavigate();
+
   return (
     <div className="container pt-4 pt-md-5">
-      <div className="row">
-        <div className="col-7">
-          <h2 className="text-success">NAME</h2>
-          <span>
-            <span
-              className="badge text-bg-dark pt-2"
-              style={{ height: "40px", fontSize: "20px" }}
-            >
-              CATEGORY
+      {!isLoading ? (
+        <div className="row">
+          <div className="col-7">
+            <h2 className="text-success">{data.result?.name}</h2>
+            <span>
+              <span
+                className="badge text-bg-dark pt-2"
+                style={{ height: "40px", fontSize: "20px" }}
+              >
+                {data.result?.category}
+              </span>
             </span>
-          </span>
-          <span>
-            <span
-              className="badge text-bg-light pt-2"
-              style={{ height: "40px", fontSize: "20px" }}
-            >
-              SPECIAL TAG
+            <span>
+              <span
+                className="badge text-bg-light pt-2"
+                style={{ height: "40px", fontSize: "20px" }}
+              >
+                {data.result?.specialTag}
+              </span>
             </span>
-          </span>
-          <p style={{ fontSize: "20px" }} className="pt-2">
-            DESCRIPTION
-          </p>
-          <span className="h3">$10</span> &nbsp;&nbsp;&nbsp;
-          <span
-            className="pb-2  p-3"
-            style={{ border: "1px solid #333", borderRadius: "30px" }}
-          >
-            <i
-              className="bi bi-dash p-1"
-              style={{ fontSize: "25px", cursor: "pointer" }}
-            ></i>
-            <span className="h3 mt-3 px-3">XX</span>
-            <i
-              className="bi bi-plus p-1"
-              style={{ fontSize: "25px", cursor: "pointer" }}
-            ></i>
-          </span>
-          <div className="row pt-4">
-            <div className="col-5">
-              <button className="btn btn-success form-control">
-                Add to Cart
-              </button>
-            </div>
+            <p style={{ fontSize: "20px" }} className="pt-2">
+              {data.result?.description}
+            </p>
+            <span className="h3">${data.result?.price}</span> &nbsp;&nbsp;&nbsp;
+            <span
+              className="pb-2  p-3"
+              style={{ border: "1px solid #333", borderRadius: "30px" }}
+            >
+              <i
+                className="bi bi-dash p-1"
+                style={{ fontSize: "25px", cursor: "pointer" }}
+              ></i>
+              <span className="h3 mt-3 px-3">XX</span>
+              <i
+                className="bi bi-plus p-1"
+                style={{ fontSize: "25px", cursor: "pointer" }}
+              ></i>
+            </span>
+            <div className="row pt-4">
+              <div className="col-5">
+                <button className="btn btn-success form-control">
+                  Add to Cart
+                </button>
+              </div>
 
-            <div className="col-5 ">
-              <button className="btn btn-secondary form-control">
-                Back to Home
-              </button>
+              <div className="col-5 ">
+                <button
+                  className="btn btn-secondary form-control"
+                  onClick={() => navigate(-1)}
+                >
+                  Back to Home
+                </button>
+              </div>
             </div>
           </div>
+          <div className="col-5">
+            <img
+              src={data.result.image}
+              width="100%"
+              style={{ borderRadius: "50%" }}
+              alt="No content"
+            ></img>
+          </div>
         </div>
-        <div className="col-5">
-          <img
-            src="https://via.placeholder.com/150"
-            width="100%"
-            style={{ borderRadius: "50%" }}
-            alt="No content"
-          ></img>
+      ) : (
+        <div
+          className="d-flex justify-content-center"
+          style={{ width: "100%" }}
+        >
+          <div>Loading...</div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
