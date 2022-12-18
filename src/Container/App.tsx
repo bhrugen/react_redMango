1f5e540a263fbf35ee4fbb9f596af84c0f712fc6
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-12-18 09:08:49 -0600

    Section 16 - Install Bootstrap and Bootstrap Icons

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function App() {
-  return <div className="App">App</div>;
+  return <div className="text-success">App</div>;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./Container/App";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
