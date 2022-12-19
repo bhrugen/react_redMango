a512f708c21ad5add5af78e949040521969193f8
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-12-19 11:13:37 -0600

    Section 19 - Cleanup test Components

--- src/Components/Layout/Header.tsx
+++ src/Components/Layout/Header.tsx
@@ -62,24 +62,6 @@ function Header() {
                   {userData.id && `(${shoppingCartFromStore.length})`}
                 </NavLink>
               </li>
-              <li className="nav-item">
-                <NavLink
-                  className="nav-link"
-                  aria-current="page"
-                  to="/authentication"
-                >
-                  Authentication
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink
-                  className="nav-link"
-                  aria-current="page"
-                  to="/authorization"
-                >
-                  Authorization
-                </NavLink>
-              </li>
 
               <li className="nav-item dropdown">
                 <a
