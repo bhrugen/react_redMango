3e08d76bace237f822cca69e895bd2464325f0a2
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-12-18 20:58:55 -0600

    Section 19 - Register User

--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
+import { useRegisterUserMutation } from "../Apis/authApi";
 import { inputHelper } from "../Helper";
+import { apiResponse } from "../Interfaces";
 import { SD_Roles } from "../Utility/SD";
 
 function Register() {
+  const [registerUser] = useRegisterUserMutation();
   const [loading, setLoading] = useState(false);
   const [userInput, setUserInput] = useState({
     userName: "",
@@ -18,9 +21,27 @@ function Register() {
     setUserInput(tempData);
   };
 
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    const response: apiResponse = await registerUser({
+      userName: userInput.userName,
+      password: userInput.password,
+      role: userInput.role,
+      name: userInput.name,
+    });
+    if (response.data) {
+      console.log(response.data);
+    } else if (response.error) {
+      console.log(response.error.data.errorMessages[0]);
+    }
+
+    setLoading(false);
+  };
+
   return (
     <div className="container text-center">
-      <form method="post">
+      <form method="post" onSubmit={handleSubmit}>
         <h1 className="mt-5">Register</h1>
         <div className="mt-5">
           <div className="col-sm-6 offset-sm-3 col-xs-12 mt-4">
