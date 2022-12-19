590622fb0b40ff33158299f95b0576310d434d8a
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-12-19 14:09:52 -0600

    Section 20 - Stripe Processing

--- src/Components/Page/Payment/PaymentForm.tsx
+++ src/Components/Page/Payment/PaymentForm.tsx
@@ -1,8 +1,41 @@
-import { PaymentElement } from "@stripe/react-stripe-js";
+import {
+  useStripe,
+  useElements,
+  PaymentElement,
+} from "@stripe/react-stripe-js";
+import { useState } from "react";
+import { toastNotify } from "../../../Helper";
 
 const PaymentForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [isProcessing, setIsProcessing] = useState(false);
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+    setIsProcessing(true);
+    const result = await stripe.confirmPayment({
+      //`Elements` instance that was used to create the Payment Element
+      elements,
+      confirmParams: {
+        return_url: "https://example.com/order/123/complete",
+      },
+    });
+
+    if (result.error) {
+      // Show error to your customer (for example, payment details incomplete)
+      toastNotify("An unexpected error occured.", "error");
+      setIsProcessing(false);
+    } else {
+      console.log(result);
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <PaymentElement />
       <button className="btn btn-success mt-5 w-100">Submit</button>
     </form>
