0c2c63dde7d90294842ed76fd2967e04bf2ae390
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-12-18 21:06:04 -0600

    Section 19 - Login in Action

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
+import { useLoginUserMutation } from "../Apis/authApi";
 import { inputHelper } from "../Helper";
+import { apiResponse } from "../Interfaces";
 
 function Login() {
+  const [error, setError] = useState("");
+  const [loginUser] = useLoginUserMutation();
   const [loading, setLoading] = useState(false);
   const [userInput, setUserInput] = useState({
     userName: "",
@@ -13,9 +17,25 @@ function Login() {
     setUserInput(tempData);
   };
 
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    const response: apiResponse = await loginUser({
+      userName: userInput.userName,
+      password: userInput.password,
+    });
+    if (response.data) {
+      console.log(response.data);
+    } else if (response.error) {
+      console.log(response.error.data.errorMessages[0]);
+      setError(response.error.data.errorMessages[0]);
+    }
+
+    setLoading(false);
+  };
   return (
     <div className="container text-center">
-      <form method="post">
+      <form method="post" onSubmit={handleSubmit}>
         <h1 className="mt-5">Login</h1>
         <div className="mt-5">
           <div className="col-sm-6 offset-sm-3 col-xs-12 mt-4">
@@ -44,6 +64,7 @@ function Login() {
         </div>
 
         <div className="mt-2">
+          {error && <p className="text-danger">{error}</p>}
           <button
             type="submit"
             className="btn btn-success"
