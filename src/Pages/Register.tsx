aa5bf83841b5835eadb19b60ae78865b6c74494c
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-12-19 08:47:31 -0600

    Section 19 - Add Loader on Register

--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -4,6 +4,7 @@ import { inputHelper, toastNotify } from "../Helper";
 import { apiResponse } from "../Interfaces";
 import { SD_Roles } from "../Utility/SD";
 import { useNavigate } from "react-router-dom";
+import { MainLoader } from "../Components/Page/Common";
 
 function Register() {
   const [registerUser] = useRegisterUserMutation();
@@ -44,6 +45,7 @@ function Register() {
 
   return (
     <div className="container text-center">
+      {loading && <MainLoader />}
       <form method="post" onSubmit={handleSubmit}>
         <h1 className="mt-5">Register</h1>
         <div className="mt-5">
@@ -95,7 +97,7 @@ function Register() {
           </div>
         </div>
         <div className="mt-5">
-          <button type="submit" className="btn btn-success">
+          <button type="submit" className="btn btn-success" disabled={loading}>
             Register
           </button>
         </div>
