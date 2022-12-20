ce8f4ef08f150195beef2ea423a54cf4b83b274e
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-12-20 15:19:21 -0600

    Section 23 - Search in Banner

--- src/Components/Page/Home/MenuItemList.tsx
+++ src/Components/Page/Home/MenuItemList.tsx
@@ -3,19 +3,46 @@ import { useState, useEffect } from "react";
 import { useGetMenuItemsQuery } from "../../../Apis/menuItemApi";
 import { menuItemModel } from "../../../Interfaces";
 import MenuItemCard from "./MenuItemCard";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setMenuItem } from "../../../Storage/Redux/menuItemSlice";
 import { MainLoader } from "../Common";
+import { RootState } from "../../../Storage/Redux/store";
 function MenuItemList() {
-  //const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
+  const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
   const dispatch = useDispatch();
   const { data, isLoading } = useGetMenuItemsQuery(null);
+
+  const searchValue = useSelector(
+    (state: RootState) => state.menuItemStore.search
+  );
+
+  useEffect(() => {
+    if (data && data.result) {
+      const tempMenuArray = handleFilters(searchValue);
+      setMenuItems(tempMenuArray);
+    }
+  }, [searchValue]);
 
   useEffect(() => {
     if (!isLoading) {
       dispatch(setMenuItem(data.result));
+      setMenuItems(data.result);
     }
   }, [isLoading]);
+
+  const handleFilters = (search: string) => {
+    let tempMenuItems = [...data.result];
+
+    //search functionality
+    if (search) {
+      const tempSearchMenuItems = [...tempMenuItems];
+      tempMenuItems = tempSearchMenuItems.filter((item: menuItemModel) =>
+        item.name.toUpperCase().includes(search.toUpperCase())
+      );
+    }
+
+    return tempMenuItems;
+  };
 
   if (isLoading) {
     return <MainLoader />;
@@ -23,8 +50,8 @@ function MenuItemList() {
 
   return (
     <div className="container row">
-      {data.result.length > 0 &&
-        data.result.map((menuItem: menuItemModel, index: number) => (
+      {menuItems.length > 0 &&
+        menuItems.map((menuItem: menuItemModel, index: number) => (
           <MenuItemCard menuItem={menuItem} key={index} />
         ))}
     </div>
