965e5977f0e615a89f9e2e0cc0d23989a2e6b79d
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-12-20 07:45:35 -0600

    Section 21 - Update Order Status

--- src/Components/Page/Order/OrderSummary.tsx
+++ src/Components/Page/Order/OrderSummary.tsx
@@ -6,12 +6,16 @@ import { useNavigate } from "react-router-dom";
 import { SD_Roles, SD_Status } from "../../../Utility/SD";
 import { RootState } from "../../../Storage/Redux/store";
 import { useSelector } from "react-redux";
+import { useState } from "react";
+import { useUpdateOrderHeaderMutation } from "../../../Apis/orderApi";
+import { MainLoader } from "../Common";
 
 function OrderSummary({ data, userInput }: orderSummaryProps) {
   const badgeTypeColor = getStatusColor(data.status!);
   const navigate = useNavigate();
   const userData = useSelector((state: RootState) => state.userAuthStore);
-
+  const [loading, setIsLoading] = useState(false);
+  const [updateOrderHeader] = useUpdateOrderHeaderMutation();
   const nextStatus: any =
     data.status! === SD_Status.CONFIRMED
       ? { color: "info", value: SD_Status.BEING_COOKED }
@@ -22,59 +26,92 @@ function OrderSummary({ data, userInput }: orderSummaryProps) {
           value: SD_Status.COMPLETED,
         };
 
+  const handleNextStatus = async () => {
+    setIsLoading(true);
+    await updateOrderHeader({
+      orderHeaderId: data.id,
+      status: nextStatus.value,
+    });
+
+    setIsLoading(false);
+  };
+
+  const handleCancel = async () => {
+    setIsLoading(true);
+    await updateOrderHeader({
+      orderHeaderId: data.id,
+      status: SD_Status.CANCELLED,
+    });
+    setIsLoading(false);
+  };
+
   return (
     <div>
-      {" "}
-      <div className="d-flex justify-content-between align-items-center">
-        <h3 className="text-success">Order Summary</h3>
-        <span className={`btn btn-outline-${badgeTypeColor} fs-6`}>
-          {data.status}
-        </span>
-      </div>
-      <div className="mt-3">
-        <div className="border py-3 px-2">Name : {userInput.name}</div>
-        <div className="border py-3 px-2">Email : {userInput.email}</div>
-        <div className="border py-3 px-2">Phone : {userInput.phoneNumber}</div>
-        <div className="border py-3 px-2">
-          <h4 className="text-success">Menu Items</h4>
-          <div className="p-3">
-            {data.cartItems?.map((cartItem: cartItemModel, index: number) => {
-              return (
-                <div className="d-flex" key={index}>
-                  <div className="d-flex w-100 justify-content-between">
-                    <p>{cartItem.menuItem?.name}</p>
-                    <p>
-                      ${cartItem.menuItem?.price} x {cartItem.quantity} =
-                    </p>
-                  </div>
-                  <p style={{ width: "70px", textAlign: "right" }}>
-                    $
-                    {(cartItem.menuItem?.price ?? 0) * (cartItem.quantity ?? 0)}
-                  </p>
-                </div>
-              );
-            })}
+      {loading && <MainLoader />}
+      {!loading && (
+        <>
+          <div className="d-flex justify-content-between align-items-center">
+            <h3 className="text-success">Order Summary</h3>
+            <span className={`btn btn-outline-${badgeTypeColor} fs-6`}>
+              {data.status}
+            </span>
+          </div>
+          <div className="mt-3">
+            <div className="border py-3 px-2">Name : {userInput.name}</div>
+            <div className="border py-3 px-2">Email : {userInput.email}</div>
+            <div className="border py-3 px-2">
+              Phone : {userInput.phoneNumber}
+            </div>
+            <div className="border py-3 px-2">
+              <h4 className="text-success">Menu Items</h4>
+              <div className="p-3">
+                {data.cartItems?.map(
+                  (cartItem: cartItemModel, index: number) => {
+                    return (
+                      <div className="d-flex" key={index}>
+                        <div className="d-flex w-100 justify-content-between">
+                          <p>{cartItem.menuItem?.name}</p>
+                          <p>
+                            ${cartItem.menuItem?.price} x {cartItem.quantity} =
+                          </p>
+                        </div>
+                        <p style={{ width: "70px", textAlign: "right" }}>
+                          $
+                          {(cartItem.menuItem?.price ?? 0) *
+                            (cartItem.quantity ?? 0)}
+                        </p>
+                      </div>
+                    );
+                  }
+                )}
 
-            <hr />
-            <h4 className="text-danger" style={{ textAlign: "right" }}>
-              ${data.cartTotal?.toFixed(2)}
-            </h4>
+                <hr />
+                <h4 className="text-danger" style={{ textAlign: "right" }}>
+                  ${data.cartTotal?.toFixed(2)}
+                </h4>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="d-flex justify-content-between align-items-center mt-3">
-        <button className="btn btn-secondary" onClick={() => navigate(-1)}>
-          Back to Orders
-        </button>
-        {userData.role == SD_Roles.ADMIN && (
-          <div className="d-flex">
-            <button className="btn btn-danger mx-2">Cancel</button>
-            <button className={`btn btn-${nextStatus.color}`}>
-              {nextStatus.value}
+          <div className="d-flex justify-content-between align-items-center mt-3">
+            <button className="btn btn-secondary" onClick={() => navigate(-1)}>
+              Back to Orders
             </button>
+            {userData.role == SD_Roles.ADMIN && (
+              <div className="d-flex">
+                <button className="btn btn-danger mx-2" onClick={handleCancel}>
+                  Cancel
+                </button>
+                <button
+                  className={`btn btn-${nextStatus.color}`}
+                  onClick={handleNextStatus}
+                >
+                  {nextStatus.value}
+                </button>
+              </div>
+            )}
           </div>
-        )}
-      </div>
+        </>
+      )}
     </div>
   );
 }
