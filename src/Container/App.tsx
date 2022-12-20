ac21f0b67abf6f873521b8828f14843750c5aa37
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-12-20 17:49:54 -0600

    Section 23 - Skip Getting Shopping Cart

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Footer, Header } from "../Components/Layout";
 import {
   AccessDenied,
@@ -30,8 +30,11 @@ import { RootState } from "../Storage/Redux/store";
 
 function App() {
   const dispatch = useDispatch();
+  const [skip, setSkip] = useState(true);
   const userData = useSelector((state: RootState) => state.userAuthStore);
-  const { data, isLoading } = useGetShoppingCartQuery(userData.id);
+  const { data, isLoading } = useGetShoppingCartQuery(userData.id, {
+    skip: skip,
+  });
 
   useEffect(() => {
     const localToken = localStorage.getItem("token");
@@ -42,10 +45,15 @@ function App() {
   }, []);
 
   useEffect(() => {
-    if (!isLoading) {
+    if (!isLoading && data) {
+      console.log(data);
       dispatch(setShoppingCart(data.result?.cartItems));
     }
   }, [data]);
+
+  useEffect(() => {
+    if (userData.id) setSkip(false);
+  }, [userData]);
 
   return (
     <div>
