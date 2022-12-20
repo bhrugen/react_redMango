9d9251d25fc0988ee465ee93d682a53e3fdcf54e
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-12-20 16:05:09 -0600

    Section 23 - Sorting in Action

--- src/Components/Page/Home/MenuItemList.tsx
+++ src/Components/Page/Home/MenuItemList.tsx
@@ -7,20 +7,31 @@ import { useDispatch, useSelector } from "react-redux";
 import { setMenuItem } from "../../../Storage/Redux/menuItemSlice";
 import { MainLoader } from "../Common";
 import { RootState } from "../../../Storage/Redux/store";
+import { SD_SortTypes } from "../../../Utility/SD";
 function MenuItemList() {
   const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [categoryList, setCategoryList] = useState([""]);
   const dispatch = useDispatch();
+  const [sortName, setSortName] = useState(SD_SortTypes.NAME_A_Z);
   const { data, isLoading } = useGetMenuItemsQuery(null);
-
+  const sortOptions: Array<SD_SortTypes> = [
+    SD_SortTypes.PRICE_LOW_HIGH,
+    SD_SortTypes.PRICE_HIGH_LOW,
+    SD_SortTypes.NAME_A_Z,
+    SD_SortTypes.NAME_Z_A,
+  ];
   const searchValue = useSelector(
     (state: RootState) => state.menuItemStore.search
   );
 
   useEffect(() => {
     if (data && data.result) {
-      const tempMenuArray = handleFilters(selectedCategory, searchValue);
+      const tempMenuArray = handleFilters(
+        sortName,
+        selectedCategory,
+        searchValue
+      );
       setMenuItems(tempMenuArray);
     }
   }, [searchValue]);
@@ -40,6 +51,16 @@ function MenuItemList() {
     }
   }, [isLoading]);
 
+  const handleSortClick = (i: number) => {
+    setSortName(sortOptions[i]);
+    const tempArray = handleFilters(
+      sortOptions[i],
+      selectedCategory,
+      searchValue
+    );
+    setMenuItems(tempArray);
+  };
+
   const handleCategoryClick = (i: number) => {
     const buttons = document.querySelectorAll(".custom-buttons");
     let localCategory;
@@ -52,7 +73,7 @@ function MenuItemList() {
           localCategory = categoryList[index];
         }
         setSelectedCategory(localCategory);
-        const tempArray = handleFilters(localCategory, searchValue);
+        const tempArray = handleFilters(sortName, localCategory, searchValue);
         setMenuItems(tempArray);
       } else {
         button.classList.remove("active");
@@ -60,7 +81,11 @@ function MenuItemList() {
     });
   };
 
-  const handleFilters = (category: string, search: string) => {
+  const handleFilters = (
+    sortType: SD_SortTypes,
+    category: string,
+    search: string
+  ) => {
     let tempArray =
       category === "All"
         ? [...data.result]
@@ -71,9 +96,31 @@ function MenuItemList() {
 
     //search functionality
     if (search) {
-      const tempSearchMenuItems = [...tempArray];
-      tempArray = tempSearchMenuItems.filter((item: menuItemModel) =>
+      const tempArray2 = [...tempArray];
+      tempArray = tempArray2.filter((item: menuItemModel) =>
         item.name.toUpperCase().includes(search.toUpperCase())
+      );
+    }
+
+    //sort
+    if (sortType === SD_SortTypes.PRICE_LOW_HIGH) {
+      tempArray.sort((a: menuItemModel, b: menuItemModel) => a.price - b.price);
+    }
+    if (sortType === SD_SortTypes.PRICE_HIGH_LOW) {
+      tempArray.sort((a: menuItemModel, b: menuItemModel) => b.price - a.price);
+    }
+    if (sortType === SD_SortTypes.NAME_A_Z) {
+      tempArray.sort(
+        (a: menuItemModel, b: menuItemModel) =>
+          a.name.toUpperCase().charCodeAt(0) -
+          b.name.toUpperCase().charCodeAt(0)
+      );
+    }
+    if (sortType === SD_SortTypes.NAME_Z_A) {
+      tempArray.sort(
+        (a: menuItemModel, b: menuItemModel) =>
+          b.name.toUpperCase().charCodeAt(0) -
+          a.name.toUpperCase().charCodeAt(0)
       );
     }
 
@@ -89,7 +136,11 @@ function MenuItemList() {
       <div className="my-3">
         <ul className="nav w-100 d-flex justify-content-center">
           {categoryList.map((categoryName, index) => (
-            <li className="nav-item" key={index}>
+            <li
+              className="nav-item"
+              style={{ ...(index === 0 && { marginLeft: "auto" }) }}
+              key={index}
+            >
               <button
                 className={`nav-link p-0 pb-2 custom-buttons fs-5 ${
                   index === 0 && "active"
@@ -100,6 +151,27 @@ function MenuItemList() {
               </button>
             </li>
           ))}
+          <li className="nav-item dropdown" style={{ marginLeft: "auto" }}>
+            <div
+              className="nav-link dropdown-toggle text-dark fs-6 border"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              {sortName}
+            </div>
+            <ul className="dropdown-menu">
+              {sortOptions.map((sortType, index) => (
+                <li
+                  key={index}
+                  className="dropdown-item"
+                  onClick={() => handleSortClick(index)}
+                >
+                  {sortType}
+                </li>
+              ))}
+            </ul>
+          </li>
         </ul>
       </div>
 
--- src/Utility/SD.ts
+++ src/Utility/SD.ts
@@ -18,3 +18,10 @@ export enum SD_Categories {
   DESSERT = "Dessert",
   BEVERAGES = "Beverages",
 }
+
+export enum SD_SortTypes {
+  PRICE_LOW_HIGH = "Price Low - High",
+  PRICE_HIGH_LOW = "Price High - Low",
+  NAME_A_Z = "Name A - Z",
+  NAME_Z_A = "Name Z - A",
+}
