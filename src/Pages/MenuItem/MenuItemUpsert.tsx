a57a9c2bd7a8cd09c79e2052d7b5f7d6c463fb20
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-12-20 13:43:12 -0600

    Section 22 - Small Bug

--- src/Pages/MenuItem/MenuItemUpsert.tsx
+++ src/Pages/MenuItem/MenuItemUpsert.tsx
@@ -102,7 +102,7 @@ function MenuItemUpsert() {
 
     formData.append("Name", menuItemInputs.name);
     formData.append("Description", menuItemInputs.description);
-    formData.append("SpecialTag", menuItemInputs.specialTag);
+    formData.append("SpecialTag", menuItemInputs.specialTag ?? "");
     formData.append("Category", menuItemInputs.category);
     formData.append("Price", menuItemInputs.price);
     if (imageToDisplay) formData.append("File", imageToStore);
